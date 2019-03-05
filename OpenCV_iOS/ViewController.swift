98b70557bd0a98bb3d31df60772cc6e60f61b962
Yejin_Cho <yy991125@naver.com>
2019-03-05 22:21:54 +0900

    Initial Commit

---
+++ OpenCV_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenCV_iOS
+//
+//  Created by 조예진 on 05/03/2019.
+//  Copyright © 2019 조예진. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
